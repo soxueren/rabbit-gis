517defe708e95d1f7df327ad4b6fe5894ea7d3e3
wenyulin.lin@gmail.com <wenyulin.lin@gmail.com@c8faddbb-1374-f190-4d65-931d2396bdaf>
2012-12-13 09:21:47 +0000

    Init

---
+++ Builds/Desktop/Desktop.cpp
@@ -0,0 +1,12 @@
+#include "Desktop.h"
+
+Desktop::Desktop(QWidget *parent, Qt::WFlags flags)
+	: QMainWindow(parent, flags)
+{
+	ui.setupUi(this);
+}
+
+Desktop::~Desktop()
+{
+
+}
---
+++ Builds/RQTCore/rqtcore.h
@@ -0,0 +1,16 @@
+#ifndef RQTCORE_H
+#define RQTCORE_H
+
+#include "rqtcore_global.h"
+
+class RQTCORE_EXPORT RQTCore
+{
+public:
+	RQTCore();
+	~RQTCore();
+
+private:
+
+};
+
+#endif // RQTCORE_H
