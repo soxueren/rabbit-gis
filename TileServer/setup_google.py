7175adc5bcdf48cc77e0b77f2bfbd206f1a9b268
wenyulin.lin@gmail.com <wenyulin.lin@gmail.com@c8faddbb-1374-f190-4d65-931d2396bdaf>
2013-08-03 15:17:15 +0000

    谷歌数据下载

---
+++ TileServer/setup_google.py
@@ -0,0 +1,45 @@
+# -*- coding:utf-8 -*-
+
+import os, sys
+from cx_Freeze import setup, Executable
+import common as cm
+
+buildPath=os.path.join(os.path.dirname(__file__), 'build')
+for root, dirs, files in os.walk(buildPath):
+    for name in files:
+	os.remove(os.path.join(root, name))
+    #for name in dirs:
+    #	os.removedirs(os.path.join(root, name))
+
+base = None
+'''
+if sys.platform == "win32":
+    base = "Win32GUI"
+'''
+
+buildOpts = dict(
+        compressed=True,
+	includes=['numpy'],
+	#zip_includes=['smSci/sci3d.sci3d'],
+	include_files=['smSci\\sci3d.sci3d','logo.png','icon.ico',
+	    'config.ini','ReadMe.txt',],
+	)
+
+msiOpts=dict(
+	add_to_path=True,
+	target_name=cm.APPNAME+'-'+cm.VERSION
+	)
+
+exeTables = [Executable("TileServer4Download.py", \
+			targetName=cm.APPNAME+'-Google2Sci3d.exe',\
+			icon='icon.ico',base=base),] 
+setup(
+        name = "TileServer",
+	version = cm.VERSION[:cm.VERSION.rfind('.')],
+        description = "Google Maps to SuperMap tile files.",
+	author = 'wenyulin.lin@gmail.com',
+        author_email = 'wenyulin.lin@gmail.com',
+        maintainer = 'linwenyu',
+        url = 'www.atolin.net',
+	options = dict(build_exe=buildOpts, bdist_msi=msiOpts),
+        executables = exeTables)
