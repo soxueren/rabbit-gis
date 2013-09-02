bdffc7a393d9b2741c99e7f3c816cc1d5f915e41
wenyulin.lin@gmail.com <wenyulin.lin@gmail.com@c8faddbb-1374-f190-4d65-931d2396bdaf>
2013-09-02 14:27:14 +0000

    二维地图缓存

--- TileServer/TileServer4DownloadSci.py
+++ TileServer/TileServer4DownloadSci.py
@@ -30,51 +30,6 @@ import common as cm
 import license as lic
 import scitemplate
 
-#---------------------------------------------------------------------------
-def appPath():
-    try:
-        dirName = os.path.dirname(os.path.abspath(__file__))
-    except:
-        dirName = os.path.dirname(os.path.abspath(sys.argv[0]))
-
-    # 打包后目录发生变化,需要去掉library.zip目录
-    dirName = dirName.replace("library.zip","")
-    return dirName
-
-#---------------------------------------------------------------------------
-def tileInGoogle(row, col, level):
-    """ 指定瓦片真的是谷歌的吗 """
-    size = 2**level
-    if row<size and col<size:
-        return True
-    return False
-
-#---------------------------------------------------------------------------
-def loadTiles(rs,re,cs,ce, level, outPath):
-    """ 将本地瓦片加载到内存 """
-
-    tiles = {}
-    gm = srsweb.GlobalMercator(256)
-    for row in range(rs, re+1):
-        for col in range(cs, ce+1):
-            if not tileInGoogle(row, col, level):
-                continue
-            outfile = "%d_%d_%d.png" % (level, row, col) 
-            fp = os.path.join(outPath, outfile)
-            if not os.path.isfile(fp):
-                continue
-
-            tmp_ds = gdal.Open(fp, gdal.GA_ReadOnly)
-            if tmp_ds is None:
-                continue
-
-            r = tmp_ds.GetRasterBand(1).ReadAsArray(0, 0, 256, 256, 256, 256) 
-            g = tmp_ds.GetRasterBand(2).ReadAsArray(0, 0, 256, 256, 256, 256) 
-            b = tmp_ds.GetRasterBand(3).ReadAsArray(0, 0, 256, 256, 256, 256) 
-            tx, ty = gm.TMSTile(col, row, level)
-            tiles[(ty,tx)] = (r,g,b) 
-            del tmp_ds
-    return tiles
 
 #---------------------------------------------------------------------------
 def downOneTile(url, fp):
@@ -89,13 +44,13 @@ def downOneTile(url, fp):
 
 
 #---------------------------------------------------------------------------
-def downlaodTile(mapname, level, row, col, outPath, strurl, ver, haswatermark):
+def downlaodTile(mapname, level, row, col, outPath, ext, strurl, ver, haswatermark):
     """ 下载瓦片到本地 """
 
     tmp = strurl 
     mkt = srsweb.GlobalMercator()
     scale = mkt.Scale(level) 
-    fp = smsci.smSci.calcTileName(outPath, mapname, scale, row, col, '.png', ver) 
+    fp = smsci.smSci.calcTileName(outPath, mapname, scale, row, col, ext, ver) 
 
     url = tmp % (random.randint(0,3), col, row, level)
     data = downOneTile(url, fp)
@@ -122,107 +77,94 @@ def downlaodTile(mapname, level, row, col, outPath, strurl, ver, haswatermark):
         myfile.write(data)
         myfile.close()
 
-#---------------------------------------------------------------------------
-def productSmTile(row, col, level, outPath, tiles, haswatermark):
-    """ 生成SuperMap瓦片 """
+    if haswatermark: # 加水印
+	tmp_ds = gdal.Open(fp, gdal.GA_ReadOnly) # 假定输入数据为24位png或jpg
+	ts  = tmp_ds.RasterXSize
 
-    mem_drv = gdal.GetDriverByName("MEM")
-    mem_ds = mem_drv.Create("", 256, 256, 3)# 输出固定为24位png或jpg
-    
-    rdata = numpy.zeros((256, 256), numpy.uint8)
-    gdata = numpy.zeros((256, 256), numpy.uint8)
-    bdata = numpy.zeros((256, 256), numpy.uint8)
-    
-    gm = srsweb.GlobalMercator(256)
-    tmp = os.path.join(outPath, "xxx")
-    l,t,r,b = smsci.smSci3d.calcBndByRowCol(row, col, level)
+        mem_drv = gdal.GetDriverByName('MEM')
+        mem_ds = mem_drv.Create('', ts, ts, 3)# 输出固定为24位png或jpg
+	for i in range(1, 4):
+	    tile_data = tmp_ds.GetRasterBand(i).ReadAsArray(0,0,ts,ts,ts,ts) 
+	    if i==1:
+		wmk.printWatermark(tile_data, wmk.buf_xsize, wmk.buf_ysize, wmk.buf_tile_server)
+	    mem_ds.GetRasterBand(i).WriteArray(tile_data)
 
-    levelRes = (180.0/256) / (1<<level) # i层分辨率
-    halfRes = levelRes*0.5
+	ext = ext[-3:].lower() 
+	if ext == 'jpg':
+	    dirv = 'JPEG'
+	elif ext == 'png':
+	    dirv = 'PNG'
 
-    for line in range(0, 256):
-        for pix in range(0, 256):
-            lat, lon = t-line*levelRes-halfRes, l+pix*levelRes+halfRes
-            x,y = gm.LatLonToMeters(lat, lon)
-            px, py = gm.MetersToPixels(x, y, level+1)
-            tx, ty = gm.PixelsToTile(px, py)
-            gx, gy = gm.GoogleTile(tx, ty, level+1)
-            #print gx,gy
-            if (ty, tx) in tiles:
-                trow, tcol = gm.PixelsPosInTile(px,py)
-                r,g,b = tiles[(ty, tx)]
-                rdata[line][pix] = r[trow][tcol]
-                gdata[line][pix] = g[trow][tcol]
-                bdata[line][pix] = b[trow][tcol]
-
-    mem_ds.GetRasterBand(1).WriteArray(rdata)
-    mem_ds.GetRasterBand(2).WriteArray(gdata)
-    mem_ds.GetRasterBand(3).WriteArray(bdata)
-
-    fp = smsci.smSci3d.calcTileName(level, row, col, outPath)+".png"
-    if not os.path.exists(os.path.dirname(fp)):
-        os.makedirs(os.path.dirname(fp))
-
-    tx, ty = mem_ds.RasterXSize, mem_ds.RasterYSize
-    tile_data = mem_ds.GetRasterBand(1).ReadAsArray(0,0,tx,ty,tx,ty) 
-    if haswatermark:
-        wmk.printWatermark(tile_data, wmk.buf_xsize, wmk.buf_ysize, wmk.buf_tile_server)
-    mem_ds.GetRasterBand(1).WriteArray(tile_data)
-
-    out_drv = gdal.GetDriverByName("PNG")
-    out_drv.CreateCopy(fp, mem_ds, strict=0)
-    del mem_ds, rdata, gdata, bdata 
+        out_drv=gdal.GetDriverByName(dirv)
+        del tmp_ds
+        out_drv.CreateCopy(fp, mem_ds, strict=0)
+        del out_drv, mem_drv
 
 #---------------------------------------------------------------------------
-def runProcess(bboxs, mapname, outPath, q, pindex, ver, haswatermark):
+def runProcess(bboxs, mapname, outPath, ext, pindex, ver, url,haswatermark):
     """ 多进程处理切图  """
-    logger = logging.getLogger("")
-    gm = srsweb.GlobalMercator(256)
-
-    url = "http://mt%d.google.cn/vt/lyrs=s@132&x=%d&y=%d&z=%d" 
     for level, row, col in bboxs:
-        downlaodTile(mapname, level, row, col, outPath, url, ver, haswatermark)
-        logger.info("%d,%d" % (row, col))
-
-def verifyLicense():
-    dirName = appPath()
-    fileList = os.listdir(dirName)
-    for fp in fileList:
-        if fp.endswith(".lic"):
-            dirName = os.path.join(dirName, fp)
-            break
-
-    pn = os.path.abspath(dirName)
-    if os.path.isfile(pn):
-        lics = lic.License(pn)
-        host = lics.hostName()
-        return lics.verify(host, cm.APPID_GOOGLE_SIC3D)
-    else:
-        return False
-
+        downlaodTile(mapname, level, row, col, outPath,ext, url, ver, haswatermark)
 
 # =============================================================================
 class Download(object):
     """ 图像信息 """
 
-    def __init__(self, taskFile=None):
-        self.task = taskFile
+    def __init__(self, argv):
         self.l, self.t, self.r, self.b = -180.0, 90.0, -180.0, 90.0
-        self.level = 0
         self.out = ""
         self.tmp = ""
         self.name = ""
         self.url = "http://mt%d.google.cn/vt/lyrs=s@132&x=%d&y=%d&z=%d" 
+	self.sm_cache_ver = smsci.VER31
+	self.file_format = 'jpg'
         self.levels = []
-        #logging = logging.getLogger("Download")
-        self.haswatermark = False if verifyLicense() else True
+        self.haswatermark = False if self.verifyLicense() else True
+        self.args = None
+        self.argparse_init(argv)
+        self.log_init()
+        self.parse_task()
 
-    def parser(self):
-        if self.task is None: return
+    def argparse_init(self, argv):
+        if self.verifyLicense():
+            msg  =strlic = "\n授权版本 %s." % lic.License.hostName()
+        else:
+            msg  = "\n免费试用版本."
 
-        if not os.path.isfile(self.task): return 
-        f = open(self.task, "r")
-        for line in f:
+        parser = argparse.ArgumentParser(description="Download GoogleMaps to SuperMap tile files",
+                epilog="Author: 42848918@qq.com"+msg)
+        parser.add_argument("-f", "--file", default="g.tsk", help="task file.")
+        self.args = parser.parse_args(argv)
+
+    def verifyLicense(self):
+        dirName = self.appPath()
+        fileList = os.listdir(dirName)
+        for fp in fileList:
+            if fp.endswith(".lic"):
+                dirName = os.path.join(dirName, fp)
+                break
+
+        pn = os.path.abspath(dirName)
+        if os.path.isfile(pn):
+            lics = lic.License(pn)
+            host = lics.hostName()
+            return lics.verify(host, cm.APPID_GOOGLE_SIC3D)
+        else:
+            return False
+
+    def parse_task(self):
+        taskfile = self.args.file
+        if not os.path.isfile(taskfile):
+            taskfile = os.path.join(self.appPath(), "g.tsk")
+        if not os.path.isfile(taskfile):
+            logging.error("任务文件未找到,%s" % taskfile)
+            return None
+
+        f = open(taskfile, "r")
+	lines = f.readlines()
+        f.close()
+
+        for line in lines:
             line = line.strip()
             if line=="" or line[0]=="#":continue
             lr = line.split("=")
@@ -257,7 +199,17 @@ class Download(object):
 
                 elif l=="name":
                     self.name = r
-        f.close()
+		elif l=="format":
+		    self.file_format= r
+		elif l=="version":
+		    if r.lower()=="ver31":
+			self.sm_cache_ver = smsci.VER31
+		    elif r.lower()=="ver30":
+			self.sm_cache_ver = smsci.VER30
+		    elif r.lower()=="ver21":
+			self.sm_cache_ver = smsci.VER21
+		    elif r.lower()=="ver20":
+			self.sm_cache_ver = smsci.VER20
 
     def splitByProcess(self, l,t,r,b, startl, endl, mpcnt):
         """ 根据进程数目,瓦片张数划分合理的任务 """
@@ -301,16 +253,18 @@ class Download(object):
         for level in range(startl, endl+1):
             scale = mkt.Scale(level)
             scales.append(scale) 
-            
 
         sci = smsci.smSci()
-        sci.setParams(self.name, mapBnd, mapBnd, smsci.VER31)
+	print self.sm_cache_ver
+        sci.setParams(self.name, mapBnd, mapBnd, self.sm_cache_ver)
         sci.setWidthHeight(w,h)
         sci.setScales(scales)
         sci.setProj(scitemplate.webmkt_prj)
+	sci.setFileFormat(self.file_format)
         sci.saveSciFile(outPath)
 
-    def doJob(self, startl, endl):
+    def run(self):
+        startl, endl = self.levels[0], self.levels[-1]
         l,t,r,b = self.l, self.t, self.r, self.b
         self.saveSciFile(l, t, r, b, startl, endl)
 
@@ -321,6 +275,7 @@ class Download(object):
         for i in xrange(len(mplist)):
             picNums += len(mplist[i])
 
+	logging.info("TaskFile:%s" % self.args.file)
         logging.info("地理范围:左上右下(%f,%f,%f,%f)" % (l,t,r,b))
         logging.info("起始终止层级:(%d,%d), 瓦片总数%d张." % (startl, endl, picNums))
 
@@ -328,10 +283,11 @@ class Download(object):
         
         plist = []
         m = mp.Manager()
-        q = m.Queue()
+
         for i in xrange(len(mplist)):
             bboxs = mplist[i]
-            p = mp.Process(target=runProcess, args=(bboxs,self.name, self.out,q,i+1,smsci.VER31,self.haswatermark))
+            p = mp.Process(target=runProcess, args=(bboxs,self.name, self.out, \
+		self.file_format,i+1,smsci.VER31,self.url,self.haswatermark))
             plist.append( (p, len(bboxs)) )
 
         for p, cnt in plist:
@@ -344,63 +300,45 @@ class Download(object):
         logging.info("End, All done.")
         logging.info(38 * "=")
 
-# =============================================================================
-def run(taskfile):
-    down = Download(taskfile)
-    down.parser()
-    startl, endl = down.levels[0], down.levels[-1]
-    down.doJob(startl, endl)
+    def log_init(self):
+	""" 初始化日志 """
+        dirName = self.appPath()
+        name = time.strftime("%Y-%m-%d.log")
+        logfile = os.path.join(dirName, "log", name)
+        logfile = os.path.abspath(logfile)
+        if not os.path.exists(os.path.dirname(logfile)):
+            os.makedirs(os.path.dirname(logfile))
+
+        logger = logging.getLogger("")
+        logger.setLevel(logging.DEBUG)
+
+        fh = logging.FileHandler(logfile)
+        fh.setLevel(logging.DEBUG)
+
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter(fmt="%(asctime)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S >")
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+
+        logger.addHandler(fh)
+        logger.addHandler(ch)
+
+        logger.info(cm.APPNAME_GOOGLE_SCI3D) 
+
+    def appPath(self):
+        try:
+            dirName = os.path.dirname(os.path.abspath(__file__))
+        except:
+            dirName = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+        # 打包后目录发生变化,需要去掉library.zip目录
+        dirName = dirName.replace("library.zip","")
+        return dirName
 
 # =============================================================================
-
-def main(taskfile):
-    dirName = appPath()
-    #name =  time.strftime("%Y-%m-%d %H-%M-%S.log")
-    name = time.strftime("%Y-%m-%d.log")
-    logfile = os.path.join(dirName, "log", name)
-    logfile = os.path.abspath(logfile)
-    if not os.path.exists(os.path.dirname(logfile)):
-        os.makedirs(os.path.dirname(logfile))
-
-    # create logger with "spam_application"
-    logger = logging.getLogger("")
-    logger.setLevel(logging.DEBUG)
-
-    # create file handler which logs even debug messages
-    fh = logging.FileHandler(logfile)
-    fh.setLevel(logging.DEBUG)
-
-    # create console handler with a higher log level
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter(fmt="%(asctime)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S >")
-    fh.setFormatter(formatter)
-    ch.setFormatter(formatter)
-
-    # add the handlers to the logger
-    logger.addHandler(fh)
-    logger.addHandler(ch)
-
-    logger.info(cm.APPNAME_GOOGLE_SCI3D) 
-    if os.path.isfile(taskfile):
-        run(taskfile)
-    else:
-        logger.error("file not found, %s" % taskfile)
-
 if __name__=="__main__":
     mp.freeze_support()
-
-    if verifyLicense():
-        msg  =strlic = "\n授权版本 %s." % lic.License.hostName()
-    else:
-        msg  = "\n免费试用版本."
-
-    parser = argparse.ArgumentParser(description="Download GoogleMaps to SuperMap tile files",
-            epilog="Author: 42848918@qq.com"+msg)
-    parser.add_argument("-f", "--file", default="g.tsk", help="task file.")
-
-    args = parser.parse_args()
-    main(args.file)
+    Download(sys.argv[1:]).run()
 
